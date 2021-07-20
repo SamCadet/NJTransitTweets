c7a06c57c0f5d6f571f7aa6ca824ece0369980af
Sam <scadet1@gmail.com>
2021-07-20 19:55:10 -0400

    Second commit

--- NJTransitTweets.py
+++ NJTransitTweets.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import time
 
 # NJTransitTweets - reveal Twitter updates on late trains with a simple search
 
@@ -16,23 +17,30 @@ api = tweepy.API(auth, wait_on_rate_limit=False,
 
 
 njtTweets = tweepy.Cursor(api.search, q="NJ Transit",
-                          until="2021-07-15", count=100).items()
+                          until="2021-07-20", count=200).items()
 
 omitWords = ['Bus', 'bus', 'Shuttle', 'shuttle', 'Bike', 'bike']
 neededWords = ['Late', 'late', 'Cancel', 'cancel',
-               'Cancelled', 'cancelled', 'Delay', 'delay', 'Delays', 'delays']
+               'Cancelled', 'cancelled', 'Delay', 'delay', 'Delays', 'delays', 'Weather', 'weather', 'Flood', 'flood', 'Mechanical', 'mechanical']
+
+
+def NJTransitTweets(userStation):
+    counter = 0
+    for tweet in njtTweets:
+        if any([word in tweet.text for word in omitWords]):
+            continue
+        if userStation.capitalize() in tweet.text and any([word in tweet.text for word in neededWords]):
+            print(str(tweet.text) + '\n')
+            counter += 1
+        if counter == 50:
+            print('Search complete, have a good day.')
+            break
+        elif userStation == 'q':
+            print('OK, have a good day.')
+            break
+
 
 userStation = input(
     'Enter the station(s) to find more info, press q to quit: ')
-counter = 0
-for tweet in njtTweets:
-    if any([word in tweet.text for word in omitWords]):
-        continue
-    if userStation.capitalize() in tweet.text and any([word in tweet.text for word in neededWords]):
-        print(tweet.text)
-        counter += 1
-    if counter == 50:
-        break
-    elif userStation == 'q':
-        print('Have a good day, #myguy.')
-        break
+
+NJTransitTweets(userStation)
