0ade17c91fad085303870f1dffaf407c500a4dcf
Sam <scadet1@gmail.com>
2021-07-26 20:19:30 -0400

    Third commit

--- NJTransitTweets.py
+++ NJTransitTweets.py
@@ -1,6 +1,6 @@
 import tweepy
 import os
-import time
+import re
 
 # NJTransitTweets - reveal Twitter updates on late trains with a simple search
 
@@ -16,31 +16,39 @@ api = tweepy.API(auth, wait_on_rate_limit=False,
                  wait_on_rate_limit_notify=False)
 
 
-njtTweets = tweepy.Cursor(api.search, q="NJ Transit",
-                          until="2021-07-20", count=200).items()
+def NJTransitTweets(userDate, userStation):
 
-omitWords = ['Bus', 'bus', 'Shuttle', 'shuttle', 'Bike', 'bike']
-neededWords = ['Late', 'late', 'Cancel', 'cancel',
-               'Cancelled', 'cancelled', 'Delay', 'delay', 'Delays', 'delays', 'Weather', 'weather', 'Flood', 'flood', 'Mechanical', 'mechanical']
+    njtTweets = tweepy.Cursor(api.search, q="NJ Transit",
+                              until=userDate, count=100).items()
 
+    omitWords = ['Bus', 'bus', 'Shuttle', 'shuttle', 'Bike', 'bike']
+    neededWords = ['Late', 'late', 'Cancel', 'cancel',
+                   'Cancelled', 'cancelled', 'Delay', 'delay', 'Delays', 'delays', 'Weather', 'weather', 'Flood', 'flood', 'Mechanical', 'mechanical']
 
-def NJTransitTweets(userStation):
     counter = 0
+
     for tweet in njtTweets:
         if any([word in tweet.text for word in omitWords]):
             continue
         if userStation.capitalize() in tweet.text and any([word in tweet.text for word in neededWords]):
             print(str(tweet.text) + '\n')
             counter += 1
-        if counter == 50:
+        if counter > 10:
             print('Search complete, have a good day.')
             break
+        elif (bool(re.match('^[0-9_-]*$', userDate))) is False:
+            print('That\'s not a valid user date, try again.')
         elif userStation == 'q':
             print('OK, have a good day.')
             break
 
 
+userDate = input(
+    'Enter today\'s date in YYYY-MM-DD format including hyphens: ')
+
 userStation = input(
     'Enter the station(s) to find more info, press q to quit: ')
+
+NJTransitTweets(userDate, userStation)
 
 NJTransitTweets(userStation)
